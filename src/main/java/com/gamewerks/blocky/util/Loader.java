c364c146b263a2c358629b268a31e5f21ba1c29f
Emily Lackershire <152826358+emilylackershire@users.noreply.github.com>
2025-02-08 20:16:16 -0600

    Update Loader.java
    fixed off by one error in loader, made program more efficient

--- src/main/java/com/gamewerks/blocky/util/Loader.java
+++ src/main/java/com/gamewerks/blocky/util/Loader.java
@@ -12,7 +12,7 @@ public class Loader {
         boolean[][] rotation = new boolean[4][4];
         for (int row = 3; row >= 0; row--) {
             String line = in.nextLine();
-            for (int col = 0; col < 5; col++) {
+            for (int col = 0; col < 4; col++) {
                 rotation[row][col] = line.charAt(col) == 'x';
             }
         }
@@ -23,21 +23,20 @@ public class Loader {
     public static boolean[][][] loadRotationData(PieceKind piece) throws IOException {
         boolean[][][] data = new boolean[4][][];
         File file = new File(Constants.DATA_PATH, piece.toString() + ".data");
-        Scanner in = new Scanner(file);
-        for (int i = 0; i < 4; i++) {
-            data[i] = readRotation(in);
-            if (in.hasNextLine()) {
-                in.nextLine();
+        try (Scanner in = new Scanner(file)) {
+            for (int i = 0; i < 4; i++) {
+                data[i] = readRotation(in);
+                if (in.hasNextLine()) {
+                    in.nextLine();
+                }
             }
         }
-        in.close();
         return data;
     }
     
     public static HashMap loadAllRotationData() throws IOException {
         HashMap ret = new HashMap();
-        for (int i = 0; i < PieceKind.ALL.length; i++) {
-            PieceKind piece = PieceKind.ALL[i];
+        for (PieceKind piece : PieceKind.ALL) {
             ret.put(piece, loadRotationData(piece));
         }
         return ret;
