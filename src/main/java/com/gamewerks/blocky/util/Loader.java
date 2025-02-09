afac7818201b5bf3a002101d3d20ff403c2dbace
Emily Lackershire <152826358+emilylackershire@users.noreply.github.com>
2025-02-09 12:52:44 -0600

    Update Loader.java
    Whoops, shuffle is NOT supposed to be here

--- src/main/java/com/gamewerks/blocky/util/Loader.java
+++ src/main/java/com/gamewerks/blocky/util/Loader.java
@@ -1,83 +1,44 @@
-package com.gamewerks.blocky;
+package com.gamewerks.blocky.util;
 
-import java.awt.Rectangle;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 
-import javax.swing.JFrame;
-
-import com.gamewerks.blocky.engine.BlockyGame;
-import com.gamewerks.blocky.engine.Direction;
 import com.gamewerks.blocky.engine.PieceKind;
-import com.gamewerks.blocky.gfx.BlockyPanel;
-import java.util.Arrays;
 
-public class Blocky {
-    private static final int FPS = 10;
-    private static final double SPF = 1000000000.0 / FPS;
-    
-    public static int[] shuffle() {
-        int [] original = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int length = original.length;
-        int [] shuffled = Arrays.copyOf(original, length);
-        int n;
-        for(int i = 0; i < length; i++) {
-            n = (int)(Math.random()* (i + 1));
-            shuffled[i] = original[n];
-            shuffled[n] = original[i];
+public class Loader {
+    private static boolean[][] readRotation(Scanner in) {
+        boolean[][] rotation = new boolean[4][4];
+        for (int row = 3; row >= 0; row--) {
+            String line = in.nextLine();
+            for (int col = 0; col < 4; col++) {
+                rotation[row][col] = line.charAt(col) == 'x';
+            }
         }
-        return shuffled;
+        return rotation;
     }
     
-    public static void main(String[] args) {
-        
-        int[] pieceArr = shuffle();
-        System.out.println(Arrays.toString(pieceArr));
-        
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Blocky");
-        
-        BlockyGame game = new BlockyGame();
-        BlockyPanel panel = new BlockyPanel(game);
-        frame.add(panel);
-        frame.pack();
-        frame.setVisible(true);
-        
-        frame.addKeyListener(new KeyAdapter() {
-            public void keyPressed(KeyEvent e) {
-                int code = e.getKeyCode();
-                if (code == e.VK_LEFT) {
-                    game.setDirection(Direction.LEFT);
-                } else if (code == e.VK_RIGHT) {
-                    game.setDirection(Direction.RIGHT);
+    
+    public static boolean[][][] loadRotationData(PieceKind piece) throws IOException {
+        boolean[][][] data = new boolean[4][][];
+        File file = new File(Constants.DATA_PATH, piece.toString() + ".data");
+        try (Scanner in = new Scanner(file)) {
+            for (int i = 0; i < 4; i++) {
+                data[i] = readRotation(in);
+                if (in.hasNextLine()) {
+                    in.nextLine();
                 }
-            }
-            
-            public void keyReleased(KeyEvent e) {
-                int code = e.getKeyCode();
-                if (code == e.VK_LEFT || code == e.VK_RIGHT) {
-                    game.setDirection(Direction.NONE);
-                } else if (code == e.VK_Z) {
-                    game.rotatePiece(false);
-                } else if (code == e.VK_X) {
-                    game.rotatePiece(true);
-                }
-            }
-        });
-        
-        long timeElapsed = 0;
-        long prevTime = System.nanoTime();
-        while (true) {
-            long currentTime = System.nanoTime();
-            timeElapsed += currentTime - prevTime;
-            prevTime = currentTime;
-            if (timeElapsed > SPF) {
-                game.step();
-                panel.paintImmediately(new Rectangle(0, 0, panel.getWidth(), panel.getHeight()));
-                timeElapsed = (long) (timeElapsed - SPF);
             }
         }
+        return data;
+    }
+    
+    public static HashMap loadAllRotationData() throws IOException {
+        HashMap ret = new HashMap();
+        for (PieceKind piece : PieceKind.ALL) {
+            ret.put(piece, loadRotationData(piece));
+        }
+        return ret;
     }
 }
